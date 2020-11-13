299699989bed43c3e208ab1e1382396d6ad1a03c
Amos98 <sintiyoamos@gmail.com>
2020-11-13 20:05:49 +0300

    Output explanation

--- lexer.py
+++ lexer.py
@@ -3,29 +3,41 @@ import re           # import for regex expressions
 tokens = []         #output in array form                         
 
 user_input = input("Enter your input: ").split() #NB: Include spaces in your input for split function to work properly.
-
+print (user_input)
 for word in user_input:
     
-    if word in ['string', 'int', 'bool','float', 'double', 'char']: 
+    bool
+    #identifies if a given string reps a data-type
+    if word in [ 'int', 'bool','float', 'double', 'char','str', ]: 
         tokens.append(['DATATYPE', word])
 
+    #identifies the loop conditions    
     elif word in ['for', 'while', 'do']:
         tokens.append(['KEYWORD:', word])
 
+    #identifies if a given string contains letters and classifies it as an identifier
     elif re.match("[a-z]", word) or re.match("[A-Z]", word):
         tokens.append(['IDENTIFIER:', word])
     
-    elif word in '*-/+%=':
+    #identifies the arithmetic operations that may exist in the inputs
+    elif word in '-/*+%=':
         tokens.append(['ARITHMETIC OPERATOR', word])
 
+    #identifies any relational operators    
     elif word in ['<', '>', '<=', '>=', '==', '!=']:
-        tokens.append(['RELATIONAL OPERATOR:', word])    
+        tokens.append(['RELATIONAL OPERATOR:', word])  
     
+    #identifies conditional operators    
+    elif word in ['&&', '|']:
+        tokens.append(['CONDITIONAL OPERATOR: ', word])
+    
+    #identifies the integers
     elif re.match(".[0-9]", word):
         if word[len(word) - 1] == ';': 
-            tokens.append(["INTEGER", word[:-1]])
-            tokens.append(['END_STATEMENT', ';'])
+            tokens.append(["INTEGER: ", word[:-1]])
         else: 
-            tokens.append(["INTEGER", word])
+            tokens.append(["INTEGER: ", word])
+    elif word in [';']
+        tokens.append(['END STATEMENT: ', word])        
 
-print(tokens) 
+print(tokens) #print the lexical analysis classification list
